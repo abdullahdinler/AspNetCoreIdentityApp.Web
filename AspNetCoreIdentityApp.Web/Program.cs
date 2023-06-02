ec27f0e65f875e18b849f5e1668e26c3e3d7f9e7
Abdullah Dinler <71527576+abdullahdinler@users.noreply.github.com>
2023-06-02 15:43:25 +0300

    4. Gün Yapılanlar: -> Cookie Configure işlemi yaptık. -> Giriş yapıldığında kullanıcının adının gösterilmesi yapıldı. -> İki farklı çıkış yapma seceneği işlendi. -> Sadece üyelerin erişe bileceği sayfalar oluşturuldu.

--- AspNetCoreIdentityApp.Web/Program.cs
+++ AspNetCoreIdentityApp.Web/Program.cs
@@ -1,7 +1,5 @@
 using AspNetCoreIdentityApp.Web.Context;
 using AspNetCoreIdentityApp.Web.Extensions;
-using AspNetCoreIdentityApp.Web.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,7 +17,26 @@ builder.Services.AddDbContext<AppDbContext>(option =>
 // Identity Configuration
 builder.Services.AddIdentityWithExtention();
 
+// Cookie Configuration
+builder.Services.ConfigureExternalCookie(opt =>
+{
+    // Burada CookieBuilder sýnýfý ile cookie ayarlarýný yapabiliriz.
+    var cookieBuilder = new CookieBuilder { Name = "AspNetCoreIdentityApp" };
+    opt.Cookie = cookieBuilder; // Cookie ayarlarýný yapýyoruz.
+    opt.LoginPath = new PathString("/Home/SignIn"); // Kullanýcý giriþ yapmadan önceki sayfa. (Login sayfasý)
+    opt.LogoutPath = new PathString("/Member/LogOut"); // Kullanýcý çýkýþ yaptýktan sonra yönlendirileceði sayfa.
+    opt.ExpireTimeSpan = TimeSpan.FromMinutes(5); // Cookie süresi
+    opt.SlidingExpiration = true; // Cookie süresi uzatýlsýn mý?
 
+
+    //opt.Cookie.Domain = "localhost"; // Cookie domain
+    //opt.Cookie.HttpOnly = true; // Cookie'ye javascript ile eriþilemesin mi?
+    //opt.Cookie.SameSite = SameSiteMode.Strict; // Cookie'ye sadece kendi domainimizden eriþilebilir.
+    //opt.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest; // Cookie'ye sadece https üzerinden eriþilebilir.
+    //opt.AccessDeniedPath = "/Home/AccessDenied"; // Kullanýcýnýn yetkisi olmayan bir sayfaya eriþmeye çalýþmasý durumunda yönlendirileceði sayfa.
+    //opt.ReturnUrlParameter = "returnUrl"; // Kullanýcýnýn yetkisi olmayan bir sayfaya eriþmeye çalýþmasý durumunda yönlendirileceði sayfa.
+   
+});
 
 
 var app = builder.Build();
@@ -36,10 +53,10 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication(); // Cookie'lerin kontrolü için kullanýlýr. ve kullanýcý giriþ yapmýþ mý diye kontrol eder.
+app.UseAuthorization(); // Yetkilendirme için kullanýlýr. 
 
-app.UseAuthorization();
-
-
+// Burada Area'lar için route tanýmlamasý yapýyoruz.
 app.MapControllerRoute(
     name: "areas",
     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
